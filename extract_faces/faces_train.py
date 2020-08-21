57c0ca3ebb2798a6294cbad1bd756d7040db4eb3
amedumer <69629296+amedumer@users.noreply.github.com>
2020-08-21 19:15:52 +0300

    Extracts faces from a given image set
    Extracts faces from a given image set and saves them under a new folder

--- extract_faces/faces_train.py
+++ extract_faces/faces_train.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 import pickle
 import time
-
+# Takes image files from images folder and saves found objects under new foler
 face_cascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_alt2.xml")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
